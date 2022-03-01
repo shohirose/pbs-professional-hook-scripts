94c00f7fd4adea543ee2dde2f54c52646e924694
Sho Hirose <sho.hirose@gmail.com>
2022-03-01 20:50:46 +0900

    fix: Error handling when traceback module is not available

--- hook_template.py
+++ hook_template.py
@@ -1,10 +1,10 @@
+from ast import Import
 import sys, os
 
 if sys.platform == 'linux' and '/usr/lib64/python3.6' not in sys.path:
     sys.path.append('/usr/lib64/python3.6')
 
 import pbs
-from traceback import TracebackException
 
 try:
     """Main Hook Script"""
@@ -14,6 +14,10 @@ except SystemExit:
     pass
 
 except Exception as err:
-    msg = list(TracebackException.from_exception(err).format())
     event = pbs.event()
-    event.reject(f'{event.hook_name} hook failed: {msg}')
+    try:
+        from traceback import TracebackException
+        msg = list(TracebackException.from_exception(err).format())
+        event.reject(f'{event.hook_name} hook failed: {msg}')
+    except ModuleNotFoundError:
+        event.reject(f'{event.hook_name} hook failed: {sys.exc_info()[:2]}')
