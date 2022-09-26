d61c45044534416d778b866981cf63a49c4bb0d2
Sho Hirose <sho.hirose@gmail.com>
2022-09-26 22:50:01 +0900

    Refactor

--- flexlm_hook_template.py
+++ flexlm_hook_template.py
@@ -3,6 +3,7 @@ import re
 import time
 import os
 
+# RHEL 7.x supports Python 3.6
 if sys.platform == 'linux' and '/usr/lib64/python3.6' not in sys.path:
     sys.path.append('/usr/lib64/python3.6')
 
@@ -10,7 +11,7 @@ import pbs
 import subprocess as sp
 from pathlib import Path
 import json
-from typing import Dict
+from typing import Dict, Tuple
 from traceback import TracebackException
 
 
@@ -33,7 +34,7 @@ class FlexlmLicenseManager:
         self.encoding = encoding
 
     @staticmethod
-    def parse_query(lines: str, feature: str) -> int:
+    def parse_query(lines: str, feature: str) -> Tuple[int, int]:
         """Parse a license query result.
 
         Args:
@@ -41,7 +42,10 @@ class FlexlmLicenseManager:
             feature (str): license feature name
 
         Returns:
-            int: The number of available license features
+            Tuple[int, int]: The number of issued and used license features
+
+        Raises:
+            ValueError: If the license feature is not found.
         """
         regex = re.compile(
             f'Users of {feature}:  \(Total of (\d+) licenses?? issued;  Total of (\d+) licenses?? in use\)')
@@ -49,20 +53,25 @@ class FlexlmLicenseManager:
 
         if not m:
             raise ValueError(
-                f'License feature is not found in Flexlm: {feature}')
+                f'License feature is not found: {feature}')
 
         issued = int(m.group(1))
         used = int(m.group(2))
-        return issued - used
+        return issued, used
 
-    def get_available_num(self, feature: str) -> int:
-        """Get the number of available license features,
+    def get_license_nums(self, feature: str) -> Tuple[int, int]:
+        """Get the number of issued and used license features,
 
         Args:
             feature (str): license feature name
 
         Returns:
-            int: The number of available license features
+            Tuple[int, int]: The number of issued and used license features
+
+        Raises:
+            ValueError: If the license feature is not found.
+            subprocess.TimeoutExpired: If timeout happened.
+            subprocess.CalledProcessError: If a called process failed.
         """
         cmd = [str(self.lmutil), 'lmstat', '-c', self.server,
                '-f', feature, '-S', self.daemon]
@@ -80,8 +89,8 @@ class HookConfig:
         license_server (str): License server
         license_daemon (str): License daemon
         stamp_file (Path): Path to the time stamp file. Default path is "/tmp/flexlm_stamp.txt".
-        interval_time (float): Interval time in seconds between license checks. Default is 10 seconds.
-        delaying_time (float): Delaying time in seconds when licenses are not available. Default is 60 seconds.
+        interval_time (float): (optional) Interval time in seconds between license checks. Default is 10 seconds.
+        delaying_time (float): (optional) Delaying time in seconds when licenses are not available. Default is 60 seconds.
     """
 
     def __init__(self, config_file: Path):
@@ -127,8 +136,11 @@ try:
         lmutil=config.lmutil, server=config.license_server, daemon=config.license_dameon)
 
     for feature, resource in resources.items():
-        required_num = job.Resource_List[resource]
-        available_num = flexlm.get_available_num(feature)
+        required_num = job.Resource_List[resource]        
+        issued_num, used_num = flexlm.get_license_nums(feature)
+        if issued_num == 0:
+            event.reject(f"License '{feature}' is not issued.")
+        available_num = issued_num - used_num
         if available_num < required_num:
             job.Execution_Time = pbs.duration(
                 time.time() + config.delaying_time)
@@ -142,6 +154,9 @@ try:
 
 except SystemExit:
     pass
+
+except (ValueError, sp.TimeoutExpired, sp.CalledProcessError) as err:
+    event.reject(f"{event.hook_name} hook failed: {err}")
 
 except Exception as err:
     event = pbs.event()
